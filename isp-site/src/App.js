b17ca2ba73a27cddcc1d45f4f53e8decd8816e20
Danny Wahl <danny@iywahl.com>
2022-12-02 16:13:05 -0700

    add inst-ui

--- isp-site/src/App.js
+++ isp-site/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { InstUISettingsProvider, instructure, Link, IconCanvasLogoLine } from '@instructure/ui'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// URLs
+const canvas = "https://github.com/thedannywahl/instructure-security-package/archive/refs/heads/canvas-lms.zip"
+
+function App() { return (
+    <InstUISettingsProvider theme={instructure}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> Edit <code>src/App.js</code> and save to reload. </p>
+          <Link href={canvas} renderIcon={<IconCanvasLogoLine size="small" />}>Download the Canvas Security Package</Link>
+        </header>
+      </div>
+    </InstUISettingsProvider>
+  )}
 
 export default App;
