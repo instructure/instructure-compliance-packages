3382b34a715a679ce102f93d3a24ec46a6bf14fb
Danny Wahl <danny@iywahl.com>
2022-12-09 09:51:57 -0700

    move to # to force hash routes

--- isp-site/src/index.js
+++ isp-site/src/index.js
@@ -1,8 +1,11 @@
+// Modules
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+// Styles
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
