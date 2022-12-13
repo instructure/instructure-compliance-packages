e7a84a538835ab6b400e7b222c3db20e32d74aa0
Danny Wahl <danny@iywahl.com>
2022-12-13 16:56:31 -0700

    more redirects

--- isp-site/src/App.js
+++ isp-site/src/App.js
@@ -23,18 +23,26 @@ function App() { return (
     <RenderTopNavBar />
     <HashRouter>
       <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={ <Root /> } />
-        <Route path="/canvas/*" element={ <CanvasRoutes /> } />
-        <Route path="/mastery/*" element={ <MasteryRoutes /> } />
-        <Route path="/impact/*" element={ <ImpactRoutes /> } />
-        <Route path="/elevate/*" element={ <ElevateRoutes /> } />
-        <Route path="/roadmap" element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Instructure-Roadmap/ct-p/instructure-roadmap' } /> } />
-        <Route path="/contact" element={ <RedirectTo url={ 'https://www.instructure.com/contact-us' } /> } />
-        <Route path="/privacy" element={ <RedirectTo url={ 'https://www.instructure.com/canvas/privacy' } /> } />
-        <Route path="/security" element={ <RedirectTo url={ 'https://www.instructure.com/products/canvas/security' } /> } />
-        <Route path="/a11y" element={ <RedirectTo url={ 'https://www.instructure.com/accessibility' } /> } />
-        <Route path="/community" element={ <RedirectTo url={ 'https://community.canvaslms.com/' } /> } />
+        <Route path="*"           element={<NotFound />} />
+        <Route path="/"           element={ <Root /> } />
+
+        {/* Product Brands */}
+        <Route path="/canvas/*"   element={ <CanvasRoutes /> } />
+        <Route path="/mastery/*"  element={ <MasteryRoutes /> } />
+        <Route path="/impact/*"   element={ <ImpactRoutes /> } />
+        <Route path="/elevate/*"  element={ <ElevateRoutes /> } />
+
+        {/* Company Redirects */}
+        <Route path="/roadmap"    element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Instructure-Roadmap/ct-p/instructure-roadmap' } /> } />
+        <Route path="/contact"    element={ <RedirectTo url={ 'https://www.instructure.com/contact-us' } /> } />
+        <Route path="/privacy"    element={ <RedirectTo url={ 'https://www.instructure.com/canvas/privacy' } /> } />
+        <Route path="/security"   element={ <RedirectTo url={ 'https://www.instructure.com/products/canvas/security' } /> } />
+        <Route path="/a11y"       element={ <RedirectTo url={ 'https://www.instructure.com/accessibility' } /> } />
+        <Route path="/community"  element={ <RedirectTo url={ 'https://community.canvaslms.com/' } /> } />
+        <Route path="/tos"        element={ <RedirectTo url={ 'https://www.instructure.com/policies/master-terms-conditions' } /> } />
+        <Route path="/tos/global" element={ <RedirectTo url={ 'https://www.instructure.com/policies/global/master-terms-and-conditions' } /> } />
+        <Route path="/aup"        element={ <RedirectTo url={ 'https://www.instructure.com/policies/acceptable-use' } /> } />
+        <Route path="/dpa"        element={ <RedirectTo url={ 'https://www.instructure.com/policies/data-processing' } /> } />
       </Routes>
     </HashRouter>
   </InstUISettingsProvider>
--- isp-site/src/routes/canvasRoutes.js
+++ isp-site/src/routes/canvasRoutes.js
@@ -7,25 +7,36 @@ import RedirectTo from 'routes/redirectTo'
 import Canvas from 'routes/canvas'
 
 const CanvasRoutes = () => (
-    <Routes>
-      <Route path="*" element={ <NotFound /> } />
-      <Route path="/" element={ <Canvas /> } />
+  <Routes>
+    <Route path="*"                   element={ <NotFound /> } />
+    <Route path="/"                   element={ <Canvas /> } />
 
-      {/* Canvas Brand */ }
-      <Route path="/community" element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas/ct-p/canvas' } /> } />
-      <Route path="/roadmap" element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas-Product-Roadmap/ct-p/canvas-product-roadmap' } /> } />
+    {/* Canvas Brand */ }
+    <Route path="/community"          element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas/ct-p/canvas' } /> } />
+    <Route path="/roadmap"            element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas-Product-Roadmap/ct-p/canvas-product-roadmap' } /> } />
 
-      {/* Canvas LMS */}
-      <Route path="/lms" element={ <RedirectTo url={ 'https://www.instructure.com/higher-education/products/canvas/canvas-lms' } /> } />
-      <Route path="/lms/k12" element={ <RedirectTo url={ 'https://www.instructure.com/k12/products/canvas/canvas-lms' } /> } />
-      <Route path="/lms/vpat" element={ <RedirectTo url={ 'https://www.instructure.com/products/canvas/accessibility' } /> } />
+    {/* Canvas LMS */}
+    <Route path="/lms"                element={ <RedirectTo url={ 'https://www.instructure.com/higher-education/products/canvas/canvas-lms' } /> } />
+    <Route path="/lms/k12"            element={ <RedirectTo url={ 'https://www.instructure.com/k12/products/canvas/canvas-lms' } /> } />
+    <Route path="/lms/vpat"           element={ <RedirectTo url={ 'https://www.instructure.com/products/canvas/accessibility' } /> } />
+    <Route path="/lms/guides"         element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas-Guides/ct-p/canvas_guides' } /> } />
 
-      {/* Canvas Studio */}
-      <Route path="/studio" element={ <RedirectTo url={ 'https://www.instructure.com/higher-education/products/canvas/canvas-studio' } /> } />
-      <Route path="/studio/k12" element={ <RedirectTo url={ 'https://www.instructure.com/k12/products/canvas/canvas-studio' } /> } />
-      <Route path="/studio/vpat" element={ <RedirectTo url={ 'https://www.instructure.com/accessibility/canvas/canvas-studio-vpat' } /> } />
+    {/* Canvas Studio */}
+    <Route path="/studio"             element={ <RedirectTo url={ 'https://www.instructure.com/higher-education/products/canvas/canvas-studio' } /> } />
+    <Route path="/studio/k12"         element={ <RedirectTo url={ 'https://www.instructure.com/k12/products/canvas/canvas-studio' } /> } />
+    <Route path="/studio/vpat"        element={ <RedirectTo url={ 'https://www.instructure.com/accessibility/canvas/canvas-studio-vpat' } /> } />
+    <Route path="/studio/guides"      element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas-Studio/ct-p/studio_guides' } /> } />
 
-    </Routes>
-  )
+    {/* Canvas Catalog */}
+    <Route path="/catalog"            element={ <RedirectTo url={ 'https://www.instructure.com/higher-education/products/canvas/canvas-catalog' } /> } />
+    <Route path="/catalog/k12"        element={ <RedirectTo url={ 'https://www.instructure.com/k12/products/canvas/canvas-catalog' } /> } />
+    <Route path="/catalog/guides"     element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas-Catalog/ct-p/canvas_catalog' } /> } />
+
+    {/* Canvas Credentials */}
+    <Route path="/credentials"        element={ <RedirectTo url={ 'https://www.instructure.com/higher-education/products/canvas/canvas-credentials-digital-badges' } /> } />
+    <Route path="/credentials/k12"    element={ <RedirectTo url={ 'https://www.instructure.com/k12/products/canvas/canvas-credentials-digital-badges' } /> } />
+    <Route path="/credentials/guides" element={ <RedirectTo url={ 'https://community.canvaslms.com/t5/Canvas-Credentials-Badges-Badgr/ct-p/canvas-credentials-badges' } /> } />
+  </Routes>
+)
 
 export default CanvasRoutes
