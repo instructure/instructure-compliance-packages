a43e0212dcee0e4b8216aa69d408d60f81a12199
Danny Wahl <danny@iywahl.com>
2022-12-14 17:17:26 -0700

    update brand links

--- isp-site/src/components/links.js
+++ isp-site/src/components/links.js
@@ -26,16 +26,21 @@ export default { data: [
                 title:  "Roadmap",
                 from:   "/canvas/roadmap",
                 to:     "https://community.canvaslms.com/t5/Canvas-Product-Roadmap/ct-p/canvas-product-roadmap"
-            }
+            },
+            {
+                title:  "Security Package (Home)",
+                from:   "/canvas",
+                to:     ""
+            },
         ]
     },
     {
         brand: "Canvas LMS",
         links: [
             {
-                title:  "Security Package (Home)",
-                from:   "/canvas",
-                to:     ""
+                title:  "Security Package (Download)",
+                from:   "/canvas/lms/dl",
+                to:     "https://github.com/thedannywahl/instructure-security-package/archive/refs/heads/canvas-lms.zip"
             },
             {
                 title:  "Community",
@@ -46,6 +51,46 @@ export default { data: [
                 title:  "Roadmap",
                 from:   "/canvas/roadmap",
                 to:     "https://community.canvaslms.com/t5/Canvas-Product-Roadmap/ct-p/canvas-product-roadmap"
+            }
+        ]
+    },
+    {
+        brand: "Canvas Studio",
+        links: [
+            {
+                title:  "Security Package (Download)",
+                from:   "/canvas/studio/dl",
+                to:     "https://github.com/thedannywahl/instructure-security-package/archive/refs/heads/canvas-studio.zip"
+            }
+        ]
+    },
+    {
+        brand: "Canvas Credentials",
+        links: [
+            {
+                title:  "Security Package (Download)",
+                from:   "/canvas/credentials/dl",
+                to:     "https://github.com/thedannywahl/instructure-security-package/archive/refs/heads/canvas-credentials.zip"
+            }
+        ]
+    },
+    {
+        brand: "Canvas Catalog",
+        links: [
+            {
+                title:  "Security Package (Download)",
+                from:   "/canvas/catalog/dl",
+                to:     "https://github.com/thedannywahl/instructure-security-package/archive/refs/heads/canvas-catalog.zip"
+            }
+        ]
+    },
+    {
+        brand: "Impact",
+        links: [
+            {
+                title:  "Security Package (Download)",
+                from:   "/impact/dl",
+                to:     "https://github.com/thedannywahl/instructure-security-package/archive/refs/heads/impact.zip"
             }
         ]
     }
