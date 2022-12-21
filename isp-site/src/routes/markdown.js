082a1ed17793b0bc2ff9bfe65b24ffec6109e9f1
Danny Wahl <danny@iywahl.com>
2022-12-21 16:30:41 -0700

    add headings components

--- isp-site/src/routes/markdown.js
+++ isp-site/src/routes/markdown.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-import { View, Link, Text, List } from '@instructure/ui'
+import { View, Link, Text, List, Heading } from '@instructure/ui'
 
 // Page
 function Markdown(props) {
@@ -31,7 +31,12 @@ function Markdown(props) {
           a:    ({node, ...props}) => <Link to={node.href} {...props} />,
           p:    ({node, ...props}) => <Text {...props} />,
           div:  ({node, ...props}) => <View as="div" {...props} />,
-          li:   ({node, ...props}) => <List.Item {...props} />
+          li:   ({node, ...props}) => <List.Item {...props} />,
+          h1:   ({node, ...props}) => { delete props.level; return(<Heading level={node.tagName} margin="none none medium" {...props} />) },
+          h2:   ({node, ...props}) => { delete props.level; return(<Heading level={node.tagName} margin="small none small" {...props} />) },
+          h3:   ({node, ...props}) => { delete props.level; return(<Heading level={node.tagName} {...props} />) },
+          h4:   ({node, ...props}) => { delete props.level; return(<Heading level={node.tagName} {...props} />) },
+          h5:   ({node, ...props}) => { delete props.level; return(<Heading level={node.tagName} {...props} />) },
         }}
       />
     </View>
