9e1db57b8b5bf90e61dc26b6585a1f495872978d
Danny Wahl <danny@iywahl.com>
2023-01-04 13:10:01 -0700

    abstract whitelisted HTML

--- isp-site/src/routes/markdown.js
+++ isp-site/src/routes/markdown.js
@@ -6,12 +6,16 @@ import remarkGemoji from 'remark-gemoji'
 import rehypeRaw from 'rehype-raw'
 import { View, Link, Text, List, Heading, SourceCodeEditor, Byline, Avatar, ToggleGroup } from '@instructure/ui'
 
+
+// Components
+import allowedElements from 'components/allowedElements'
+
 // Page
 function Markdown(props) {
   let md = props.readme
 
   const [content, setContent] = useState('')
-
+  
   useEffect(() => {
     fetch(md)
       .then((response) => {
@@ -30,6 +34,7 @@ function Markdown(props) {
     )
     return filteredChildren
   }
+
   const filterChildrenNode = (node) => {
     const filteredChildren = node.children.filter(
       (child) => child.type !== "text"
@@ -37,117 +42,108 @@ function Markdown(props) {
     return filteredChildren
   }
 
-  const allowedEls = [
-    'a', 'p', 'span', 'code', 'del', 'blockquote',
-    'h1', 'h2', 'h3', 'h4', 'h5', 'img',
-    'ul', 'li', 'div', 'pre', 'ul', 'ol', 'li', 'input',
-    'details', 'summary', 'br', 'em', 'strong', 'hr',
-    'table', 'tbody', 'thead', 'td', 'th', 'tr'
-  ]
-
   return ( 
     <View as="div">
       <ReactMarkdown
         children={content}
         remarkPlugins={[remarkGfm, remarkGemoji]}
         rehypePlugins={[rehypeRaw]}
-        allowedElements={allowedEls}
+        allowedElements={allowedElements}
         components={{
-          a:          ({node, ...props}) => <Link to={node.href} {...props} />,
-          p:          ({node, ...props}) => <Text as={node.tagName} {...props} />,
-          span:       ({node, ...props}) => <Text as={node.tagName} {...props} />,
-          code:       ({node, ...props}) => <Text as={node.tagName} {...props} />,
-          del:        ({node, ...props}) => <Text as={node.tagName} {...props} />,
+          a: ({node, ...props}) => <Link to={node.href} {...props} />,
+          p: ({node, ...props}) => <Text as={node.tagName} {...props} />,
+          em: ({node, ...props}) => <Text fontStyle="italic" {...props} />,
+          strong: ({node, ...props}) => <Text weight="bold" {...props} />,
+          span: ({node, ...props}) => <Text {...props} />,
+          code: ({node, ...props}) => <Text as={node.tagName} {...props} />,
+          del: ({node, ...props}) => <Text as={node.tagName} {...props} />,
           blockquote: ({node, ...props}) => { 
-                                              node = {...node, children: filterChildrenNode(node)}
-                                              props = { ...props, children: filterChildrenProps(props) }
-                                              const quoteArr = props.children[0].props.children[0].split("--", 2)
-                                              const quote = quoteArr[0]
-                                              const author = (quoteArr.length > 1) ? quoteArr[1] : null
-                                              return(
-                                                  <Byline description={quote}  title={author} margin="medium 0" {...props}  >
-                                                    { author ? <Avatar name={author} /> : null}
-                                                  </Byline>
-                                              )
-                                            },
-          h1:         ({node, ...props}) => {
-                                              props.level = node.tagName
-                                              return <Heading margin="none none medium" {...props} />
-                                            },
-          h2:         ({node, ...props}) => {
-                                              props.level = node.tagName;
-                                              return <Heading margin="small none small" {...props} />
-                                            },
-          h3:         ({node, ...props}) => {
-                                              props.level = node.tagName
-                                              return <Heading {...props} />
-                                            },
-          h4:         ({node, ...props}) => {
-                                              props.level = node.tagName
-                                              return <Heading {...props} />
-                                            },
-          h5:         ({node, ...props}) => {
-                                              props.level = node.tagName
-                                              return <Heading {...props} />
-                                            },
-
-          div:        ({node, ...props}) => <View as={node.tagName} {...props} />,
-          pre:        ({node, ...props}) => {
-                                              if (node.children.length === 1 && node.children[0].tagName === 'code') {
-                                                let content = node.children[0].children[0].value
-                                                if (content.endsWith("\n")) {
-                                                  content = content.slice(0, -1)
-                                                }
-                                                return (
-                                                  <SourceCodeEditor
-                                                    label='editable code editor'
-                                                    lineNumbers={true}
-                                                    foldGutter={true}
-                                                    editable={true}
-                                                    readOnly={true}
-                                                    defaultValue={content}
-                                                  />
-                                                )
-                                              } else {
-                                                return <View as={node.tagName} {...props} />
-                                              }
-                                            },
-
-          ul:         ({node, ...props}) => {
-                                              node = {...node, children: filterChildrenNode(node)}
-                                              props = { ...props, children: filterChildrenProps(props) }
-                                              return <List isUnstyled={(props.className === "contains-task-list") ? true : false} {...props} />
-                                            },
-          ol:         ({node, ...props}) => {
-                                              node = {...node, children: filterChildrenNode(node)}
-                                              props = { ...props, children: filterChildrenProps(props) }
-                                              return <List as={node.tagName}  {...props} />
-                                            },
-          li:         ({node, ...props}) => {
-                                              return(
-                                                <List.Item {...props} />
-                                              )
-                                            },
-
-          input:      ({node, ...props}) => {
-                                              switch(props.type) {
-                                                default:
-                                                  return <node.tagName {...props} />
-                                              }
-                                            },
-          details:    ({node, ...props}) => {
-                                              node = {...node, children: filterChildrenNode(node)}
-                                              props = { ...props, children: filterChildrenProps(props) }
-                                              const label = props.children.shift()
-                                              return(
-                                                <ToggleGroup
-                                                toggleLabel="Click for details"
-                                                summary={label}
-                                                >
-                                                  <View display="block" padding="small" {...props} />
-                                                </ToggleGroup>
-                                              )
-                                            }
+            node = {...node, children: filterChildrenNode(node)}
+            props = { ...props, children: filterChildrenProps(props) }
+            const quoteArr = props.children[0].props.children[0].split("--", 2)
+            const quote = quoteArr[0]
+            const author = (quoteArr.length > 1) ? quoteArr[1] : null
+            return(
+                <Byline description={quote}  title={author} margin="medium 0" {...props}  >
+                  <>{ author ? <Avatar name={author} /> : null}</>
+                </Byline>
+            )
+          },
+          h1: ({node, ...props}) => {
+            props.level = node.tagName
+            return <Heading margin="none none medium" {...props} />
+          },
+          h2: ({node, ...props}) => {
+            props.level = node.tagName;
+            return <Heading margin="small none small" {...props} />
+          },
+          h3: ({node, ...props}) => {
+            props.level = node.tagName
+            return <Heading {...props} />
+          },
+          h4: ({node, ...props}) => {
+            props.level = node.tagName
+            return <Heading {...props} />
+          },
+          h5: ({node, ...props}) => {
+            props.level = node.tagName
+            return <Heading {...props} />
+          },
+          h6: ({node, ...props}) => {
+            props.level = 'h5'
+            return <Heading as="h6" {...props} />
+          },
+          div: ({node, ...props}) => <View as={node.tagName} {...props} />,
+          pre: ({node, ...props}) => {
+            if (node.children.length === 1 && node.children[0].tagName === 'code') {
+              let content = node.children[0].children[0].value
+              if (content.endsWith("\n")) {
+                content = content.slice(0, -1)
+              }
+              return (
+                <SourceCodeEditor
+                  label='editable code editor'
+                  lineNumbers={true}
+                  foldGutter={true}
+                  editable={true}
+                  readOnly={true}
+                  defaultValue={content}
+                />
+              )
+            } else {
+              return <View as={node.tagName} {...props} />
+            }
+          },
+          ul: ({node, ...props}) => {
+            node = {...node, children: filterChildrenNode(node)}
+            props = { ...props, children: filterChildrenProps(props) }
+            return <List isUnstyled={(props.className === "contains-task-list") ? true : false} {...props} />
+          },
+          ol: ({node, ...props}) => {
+            node = {...node, children: filterChildrenNode(node)}
+            props = { ...props, children: filterChildrenProps(props) }
+            return <List as={node.tagName}  {...props} />
+          },
+          li: ({node, ...props}) => <List.Item {...props} />,
+          input:  ({node, ...props}) => {
+            switch(props.type) {
+              default:
+                return <node.tagName {...props} />
+            }
+          },
+          details: ({node, ...props}) => {
+            node = {...node, children: filterChildrenNode(node)}
+            props = {...props, children: filterChildrenProps(props)}
+            const label = props.children.shift()
+            return(
+              <ToggleGroup
+                toggleLabel="Click for details"
+                summary={label}
+              >
+                <View display="block" padding="small" {...props} />
+              </ToggleGroup>
+            )
+          }
         }}
       />
     </View>
