10a56e6dd6bf620eb3c18edb5c402e58f1750a69
Danny Wahl <danny@iywahl.com>
2024-01-05 06:43:29 -0700

    add globalBrand to body class
    toggle function now adds global brand name as a class to the body, or removes it when not viewing a brand.

--- isp-site/src/components/toggle.js
+++ isp-site/src/components/toggle.js
@@ -1,27 +1,34 @@
+import { globalBrands } from "variables/brands";
+const body = document.querySelector("body");
+
 const toggle = (u, n) => {
-	n.forEach((e) => {
+	for (const e of n) {
 		e.removeAttribute("aria-current");
 		e.classList.remove("active");
-	});
+	}
 
 	if (u !== null && u.hash !== "#/") {
 		u.setAttribute("aria-current", "page");
 		u.classList.add("active");
 	}
+
+	//TODO ADD Brand to body classList
+	body.classList.remove(...globalBrands.map((brand) => brand.toLowerCase()));
+	if (u.id.length) body.classList.add(u.id);
 };
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 	let u = document.location.href.split("#")[1];
-	let n = document.querySelectorAll("nav a");
+	const n = document.querySelectorAll("nav a");
 
 	if (u !== undefined && u !== "/" && u !== null) {
 		u = document.getElementById(u.split("/")[1]);
 		toggle(u, n);
 	}
 
-	n.forEach((el) => {
-		el.onclick = function () {
-			toggle(el, n);
+	for (const e of n) {
+		e.onclick = () => {
+			toggle(e, n);
 		};
-	});
+	}
 });
