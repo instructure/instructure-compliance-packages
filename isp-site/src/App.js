5441af7cbf048dd364578aea66949e1f448fe7d0
Danny Wahl <danny@iywahl.com>
2024-02-07 21:21:39 -0700

    prep for translation support

--- isp-site/src/App.js
+++ isp-site/src/App.js
@@ -1,6 +1,7 @@
 // Modules
 import { HashRouter } from "react-router-dom";
 import { InstUISettingsProvider, canvas, View } from "@instructure/ui";
+import { getLang } from "variables/langs";
 
 // Components
 import RenderTopNavBar from "components/RenderTopNavBar";
@@ -15,16 +16,23 @@ import RootRoutes from "routes/rootRoutes";
 
 // App
 function App() {
+	const lang = getLang(window.location.hash.split("/").pop());
+	const css = `.markdown .lang { display: none; } .markdown .lang.${lang.toUpperCase()} { display: inherit; }`;
+
 	return (
-		<InstUISettingsProvider theme={canvas}>
-			<View as="div" minHeight="100vh" position="relative">
-				<RenderTopNavBar />
-				<HashRouter future={{ v7_startTransition: true }}>
-					<RootRoutes />
-				</HashRouter>
-				<RenderFooter />
-			</View>
-		</InstUISettingsProvider>
+		<>
+			<style>{css}</style>
+			<InstUISettingsProvider theme={canvas}>
+				<View as="div" minHeight="100vh" position="relative">
+					<div>lang: {lang}</div>
+					<RenderTopNavBar l={lang} />
+					<HashRouter future={{ v7_startTransition: true }}>
+						<RootRoutes l={lang} />
+					</HashRouter>
+					<RenderFooter l={lang} />
+				</View>
+			</InstUISettingsProvider>
+		</>
 	);
 }
 
