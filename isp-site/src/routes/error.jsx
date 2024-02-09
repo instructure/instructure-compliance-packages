f2c63d7294e5635c6b5bbda3608c4794e5ce82a9
Danny Wahl <danny@iywahl.com>
2024-02-09 09:48:52 -0700

    stringify error page

--- isp-site/src/routes/error.jsx
+++ isp-site/src/routes/error.jsx
@@ -1,12 +1,27 @@
 // Modules
-import { useRouteError } from "react-router-dom";
+import { useParams, useRouteError } from "react-router-dom";
 import { View, Billboard, Text, Link } from "@instructure/ui";
+import { strings } from "strings/error";
+import { getLang, getStrings } from "variables/langs";
+import RenderTopNavBar from "components/RenderTopNavBar";
+import RenderFooter from "components/RenderFooter";
 
 // Page
 export default function ErrorPage() {
+	const l = getLang(useParams().language);
+	const s = getStrings(strings, l);
 	const err = useRouteError();
 	console.error(err);
+	let msg = s.not_your_fault;
+	if(err) {
+		msg = `
+			${err.status ? `${err.status} ${err.statusText}<br/>` : ""}
+			${err.message ? err.message : s.not_your_fault}
+		`
+	}
 	return (
+		<>
+			<RenderTopNavBar lang={l} />
 			<View
 				id="main"
 				as="div"
@@ -17,21 +32,21 @@ export default function ErrorPage() {
 			>
 				<Billboard
 					size="medium"
-					heading="Well, this is awkward."
+					heading={s.awkward}
 					headingAs="h1"
 					message={
 						<>
 							<Text as="p">
-								<i>{`${err.status} ${err.statusText}`}</i>
-								<br/>
-								<i>{err.message}</i>
+								<i>{msg}</i>
 							</Text>
 							<Text as="p">
-								Uhm... <Link href="./#/">class dismissed</Link>.
+								{s.uhm} <Link href="./#/">{s.class_dismissed}</Link>.
 							</Text>
 						</>
 					}
 				/>
 			</View>
+			<RenderFooter lang={l} />
+		</>
 	);
 }
