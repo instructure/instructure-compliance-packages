324ceb44bbd95cef530941bbda7f36080a7bfa48
Danny Wahl <danny@iywahl.com>
2024-02-09 13:04:37 -0700

    move lang utils

--- isp-site/src/variables/langs.js
+++ isp-site/src/variables/langs.js
@@ -22,13 +22,3 @@ export const globalLangDetails = [
 		code: "DE",
 	},
 ];
-
-export function getLang(lang = "EN") {
-	return globalLangs.find((e) => e.includes(lang.toUpperCase())) || "EN";
-}
-
-export function getStrings(strs, lang = "EN") {
-	return Object.fromEntries(
-		Object.entries(strs).map(([key, value]) => [key, value[lang] || value.EN]),
-	);
-}
