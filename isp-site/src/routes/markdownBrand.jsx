64aec2b24e7912549fda7d86d1f9fea5d11afd52
Danny Wahl <danny@iywahl.com>
2024-03-01 16:04:40 -0700

    Deploying version 2.1.2+1709334280

--- isp-site/src/routes/markdownBrand.jsx
+++ isp-site/src/routes/markdownBrand.jsx
@@ -1,5 +1,5 @@
+// Modules
 import React, { useState, useEffect } from "react";
-import * as ReactDOM from "react-dom/client";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkGemoji from "remark-gemoji";
@@ -15,6 +15,7 @@ import { useParams } from "react-router-dom";
 import strings from "strings/markdown";
 import { getStrings, getLang } from "utils/langs";
 import { Explorer } from "utils/explorer";
+import { createPortal } from "react-dom";
 
 // Page
 export default function MarkdownBrand({ readme, brand }) {
@@ -22,6 +23,7 @@ export default function MarkdownBrand({ readme, brand }) {
 	const s = getStrings(strings, l);
 	const css = `.markdown .lang { display: none; } .markdown .lang.${l.toUpperCase()} { display: inherit; }`;
 	const md = readme;
+
 	const [content, setContent] = useState(`${s.loading}`);
 
 	useEffect(() => {
@@ -42,10 +44,11 @@ export default function MarkdownBrand({ readme, brand }) {
 
 	useEffect(() => {
 		const branches = document.querySelectorAll(".markdown .contents");
+
 		if (branches.length > 0) {
 			for (const branch of branches) {
 				Explorer(brand.toLowerCase(), branch, l).then((table) => {
-					ReactDOM.createRoot(branch).render(
+					createPortal(
 						<Markdown
 							children={table}
 							remarkPlugins={[remarkGfm, remarkGemoji]}
@@ -53,6 +56,7 @@ export default function MarkdownBrand({ readme, brand }) {
 							allowedElements={allowedElements}
 							components={mdtoui}
 						/>,
+						branch,
 					);
 				});
 			}
@@ -71,7 +75,6 @@ export default function MarkdownBrand({ readme, brand }) {
 				margin="0 auto"
 			>
 				<style>{css}</style>
-
 				<View as="div" className="markdown">
 					<Markdown
 						children={content}
