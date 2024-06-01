713e88d38a2184cbe061857f674593e9ebb738fe
Danny Wahl <danny@iywahl.com>
2024-06-01 08:48:17 -0600

    convert mdtoui Component to tsx

--- src/components/mdtoui.tsx
+++ src/components/mdtoui.tsx
@@ -1,6 +1,5 @@
+import React, { ReactNode } from "react";
 import { Children } from "react";
-
-// Modules
 import {
   Alert,
   Avatar,
@@ -17,25 +16,26 @@ import {
   ToggleDetails,
   View,
 } from "@instructure/ui";
+import type { AsElementType } from "@instructure/shared-types";
 
-const filterChildrenProps = (props) => {
+const filterChildrenProps = (props: { children: unknown }) => {
   return Children.map(props.children, (child) =>
     typeof child !== "string" ? child : null,
   );
 };
 
-const alignStr = (p) => {
+const alignStr = (p: { style?: { textAlign?: string } }): string => {
   return (
-    p?.style?.textAlign.replace(/\b(left|right)\b/, (s) =>
+    p?.style?.textAlign?.replace(/\b(left|right)\b/, (s) =>
       s === "left" ? "start" : "end",
     ) ?? "start"
   );
 };
 
-const mdtoui = {
+const mdtoui: MarkdownCustomRenderer = {
   hr: ({ node, ...props }) => (
     <View
-      as={node.tagName}
+      as={node?.tagName as AsElementType}
       shadow="topmost"
       borderWidth="small"
       margin="small none"
@@ -48,7 +48,9 @@ const mdtoui = {
   button: ({ node, ...props }) => <Button withBackground={false} {...props} />,
   p: ({ node, ...props }) => <Text as={node.tagName} {...props} />,
   em: ({ node, ...props }) => <Text fontStyle="italic" {...props} />,
-  strong: ({ node, ...props }) => <Text weight="bold" {...props} />,
+  strong: ({ node, ...props }: { node: Text }) => (
+    <Text weight="bold" {...props} />
+  ),
   span: ({ node, ...props }) => <Text as={node.tagName} {...props} />,
   code: ({ node, ...props }) => <Text as={node.tagName} {...props} />,
   del: ({ node, ...props }) => <Text as={node.tagName} {...props} />,
@@ -58,21 +60,28 @@ const mdtoui = {
       children: filterChildrenProps(props),
     };
     const alertRegex = /\[\!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]/;
-    const alertTypes = {
-      NOTE: "info",
-      TIP: "success",
-      IMPORTANT: "info",
-      WARNING: "warning",
-      CAUTION: "error",
-    };
-    const [quote, author] =
-      Children.toArray(props.children)[0]
-        ?.props.children.toString()
-        .split("--", 2) ?? false;
+
+    const alertTypes: Record<string, "info" | "success" | "warning" | "error"> =
+      {
+        NOTE: "info",
+        TIP: "success",
+        IMPORTANT: "info",
+        WARNING: "warning",
+        CAUTION: "error",
+      };
+    const firstChild = Children.toArray(props.children)[0];
+    let quote: string;
+    let author: string;
+
+    if (React.isValidElement(firstChild)) {
+      [quote, author] = firstChild.props.children.toString().split("--", 2);
+    } else {
+      [quote, author] = firstChild.toString().split("--", 2);
+    }
     const alert = quote.match(alertRegex);
     if (alert) {
       return (
-        <Alert variant={alertTypes[alert[1]]}>
+        <Alert variant={alertTypes[alert[1] as keyof typeof alertTypes]}>
           {quote.replace(`${alert[0]}`, "")}
         </Alert>
       );
@@ -83,37 +92,24 @@ const mdtoui = {
       </Byline>
     );
   },
-  h1: ({ node, ...props }) => {
-    props.level = node.tagName;
-    return <Heading margin="none none medium" {...props} />;
-  },
-  h2: ({ node, ...props }) => {
-    props.level = node.tagName;
-    return <Heading margin="small none" {...props} />;
-  },
-  h3: ({ node, ...props }) => {
-    props.level = node.tagName;
-    return <Heading margin="small none" {...props} />;
-  },
-  h4: ({ node, ...props }) => {
-    props.level = node.tagName;
-    return <Heading {...props} />;
-  },
-  h5: ({ node, ...props }) => {
-    props.level = node.tagName;
-    return <Heading {...props} />;
-  },
-  h6: ({ node, ...props }) => {
-    props.level = "h5";
-    return <Heading as="h6" {...props} />;
-  },
-  img: ({ node, ...props }) => <Img {...props} />,
+  h1: ({ node, ...props }) => (
+    <Heading margin="none none medium" level={node.tagName} {...props} />
+  ),
+  h2: ({ node, ...props }) => (
+    <Heading margin="small none" level={node.tagName} {...props} />
+  ),
+  h3: ({ node, ...props }) => (
+    <Heading margin="small none" level={node.tagName} {...props} />
+  ),
+  h4: ({ node, ...props }) => <Heading level={node.tagName} {...props} />,
+  h5: ({ node, ...props }) => <Heading level={node.tagName} {...props} />,
+  h6: ({ node, ...props }) => <Heading as="h6" {...props} />,
+  img: ({ node, ...props }) => <Img src={node.src} {...props} />,
   div: ({ node, ...props }) => <View as={node.tagName} {...props} />,
   pre: ({ node, ...props }) => {
     if (node.children.length === 1 && node.children[0].tagName === "code") {
       let preContent = node.children[0].children[0].value;
       if (preContent.endsWith("\n")) preContent = preContent.slice(0, -1);
-
       return (
         <SourceCodeEditor
           label="Code"
@@ -130,13 +126,18 @@ const mdtoui = {
   ul: ({ node, ...props }) => {
     props = { ...props, children: filterChildrenProps(props) };
     const { children, ...ulProps } = props;
-    const tasklist = ulProps?.className === "contains-task-list" || false;
+    /**
+     * @todo: cast { children, ...ulProps } type to match { node, ...props } type
+     */
+    const tasklist: boolean =
+      // @ts-ignore
+      ulProps?.className === "contains-task-list" || false;
     return (
       <List isUnstyled={tasklist} {...ulProps}>
         {Children.map(children, (child) => {
           const { children, ...liProps } = child.props;
           if (tasklist && children) {
-            let checked;
+            let checked = false;
             return (
               <List.Item key={child.key} margin="0 0 small small" {...liProps}>
                 {Children.map(children, (child) => {
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,3 +6,18 @@ declare module "*?raw" {
   const content: string;
   export default content;
 }
+
+/**
+ * https://stackoverflow.com/questions/66628783/react-markdown-type-for-paragraph-renderer
+ *
+ */
+declare type MarkdownNodeToProps<T> = {
+  node: T;
+  children: T extends { children: unknown } ? ReactNode : never;
+};
+
+declare type MarkdownCustomRenderer = {
+  [K in Content["type"]]?: (
+    props: MarkdownNodeToProps<Extract<Content, { type: K }>>,
+  ) => ReactElement;
+};
