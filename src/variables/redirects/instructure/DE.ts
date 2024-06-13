00804bd4993b32312828298682d62151761de408
Danny Wahl <danny@iywahl.com>
2024-06-13 13:24:31 -0600

    fix contact redirect

--- src/variables/redirects/instructure/DE.ts
+++ src/variables/redirects/instructure/DE.ts
@@ -5,15 +5,10 @@ const DE: RedirectLinkPartial[] = [
     to: "https://www.instructure.com/de",
   },
   {
-    title: "Contato",
-    from: "/pt-br/contato",
-    to: "https://www.instructure.com/pt-br/contato/",
-  },
-  {
     title: "Kontakt",
-    from: "/kontakt",
+    from: "/de/kontakt",
     to: "https://www.instructure.com/de/contact-us",
   },
 ];
 
-export default DE.map((links) => ({ ...links, lang: "DE" }));
+export default DE.map((links) => ({ ...links, lang: "DE" })) as RedirectLink[];
