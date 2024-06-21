6e31c8eaa1735ca602085ac312049cbccd8cc164
Danny Wahl <danny@iywahl.com>
2024-06-21 09:18:48 -0600

    update globals

--- src/variables/globals.ts
+++ src/variables/globals.ts
@@ -1,8 +1,8 @@
 import packageJson from "../../package.json" with { type: "json" };
 
 const global: Global = {
-  owner: "thedannywahl",
-  repo: "instructure-security-package",
+  owner: "instructure",
+  repo: "compliance-packages",
   branch: "main",
   url: packageJson.homepage.replace("/#/", ""),
   api: "https://api.github.com",
