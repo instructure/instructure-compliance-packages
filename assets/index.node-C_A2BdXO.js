12bdc395a632321d9043acfcea629de21ba1ef00
Danny Wahl <danny@iywahl.com>
2024-07-10 13:14:04 -0700

    Deploy v4.4.0+1720642421

---
+++ assets/index.node-C_A2BdXO.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-D8uc6p-q.js";class o extends n.Component{constructor(e){super(e)}hydrate(){}initChange(){}resolveChange(){}mirrorChange(){}componentDidMount(){}componentDidUpdate(){}componentWillUnmount(){}shouldComponentUpdate(){return!1}render(){return null}}o.displayName="Controlled";export{o as default};
