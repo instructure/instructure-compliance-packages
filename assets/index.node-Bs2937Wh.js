d0c1f4f73feef41b57668df1054b7f4ef84ad8de
Danny Wahl <danny@iywahl.com>
2024-08-02 09:01:31 -0600

    Deploy v4.5.1+1722610868

---
+++ assets/index.node-Bs2937Wh.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-DrSsoCu-.js";class o extends n.Component{constructor(e){super(e)}hydrate(){}initChange(){}resolveChange(){}mirrorChange(){}componentDidMount(){}componentDidUpdate(){}componentWillUnmount(){}shouldComponentUpdate(){return!1}render(){return null}}o.displayName="Controlled";export{o as default};
