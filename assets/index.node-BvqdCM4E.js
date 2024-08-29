b7c4533a1601de84dc591482abb23a9955832db4
Danny Wahl <dwahl@instructure.com>
2024-08-29 08:34:11 -0600

    Deploy v5.0.1+1724942027

---
+++ assets/index.node-BvqdCM4E.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-mQ6522B8.js";class o extends n.Component{constructor(e){super(e)}hydrate(){}initChange(){}resolveChange(){}mirrorChange(){}componentDidMount(){}componentDidUpdate(){}componentWillUnmount(){}shouldComponentUpdate(){return!1}render(){return null}}o.displayName="Controlled";export{o as default};
