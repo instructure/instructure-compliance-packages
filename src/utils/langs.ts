2e4c160f522b9906340bed94e84789648280de1d
Danny Wahl <dwahl@instructure.com>
2024-09-16 15:23:12 -0600

    update docs

--- src/utils/langs.ts
+++ src/utils/langs.ts
@@ -1,21 +1,22 @@
 import { globalLangs } from "../variables/langs.ts";
 
 /**
- * Retrieves the language code from the global languages.
+ * Retrieves the language code from the global language list.
  *
- * @param lang - The language code to retrieve. Defaults to "EN".
- * @returns The language code if it exists in the global languages, otherwise "EN".
+ * @param {LangCode} [lang="EN"] - The language code to search for. Defaults to "EN".
+ * @returns {string} The matched language code from the global language list, or "EN" if not found.
  */
-export function getLang(lang: LangCode = "EN") {
+export function getLang(lang: LangCode = "EN"): string {
   return globalLangs.find((e) => e.includes(lang.toUpperCase())) || "EN";
 }
 
 /**
- * Retrieves the strings for a specific language from a page strings object.
+ * Retrieves a set of strings for a specified language from the given PageStrings object.
+ * If the specified language is not available, it defaults to English ("EN").
  *
- * @param strs - The page strings object to retrieve the strings from.
+ * @param strs - An object containing strings for different languages.
  * @param lang - The language code to retrieve the strings for. Defaults to "EN".
- * @returns An object with the same keys as the input object, but the values are the strings for the specified language, or for English if the specified language is not available.
+ * @returns An object with the strings for the specified language.
  */
 export function getStrings(strs: PageStrings, lang: LangCode = "EN") {
   return Object.fromEntries(
