808e42722bbb3f269b7adab9ead46a56af711e56
Danny Wahl <danny@iywahl.com>
2024-11-07 10:05:30 -0700

    Deploy v5.0.3+1730999095

--- assets/index.node-CMt7ghCK.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-3aPu6kFZ.js";class n extends e.Component{constructor(e){super(e)}hydrate(){}initChange(){}resolveChange(){}mirrorChange(){}componentDidMount(){}componentDidUpdate(){}componentWillUnmount(){}shouldComponentUpdate(){return!1}render(){return null}}n.displayName="Controlled";export{n as default};
