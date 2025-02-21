70f26d78ab60c50263bd8f96d4cb7473c2fbfc98
Danny Wahl <danny@iywahl.com>
2025-02-21 07:15:30 -0700

    add missing type declarations

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 interface Redirect { from: string; to: string; }
-const LinksCSV =
+const LinksCSV: string =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQXWM1qxpmOn2nbubgaf2qncwNvfSpKBkgPpVSBIjloXNTnAlEOqmr9ylgqO82iIC7pEeVfD_TI8xmd/pub?gid=1670948929&single=true&output=csv";
-const Conveyor =
+const Conveyor: string =
   "https://app.conveyor.com/profile/b61af872-3391-492e-af74-85cc6ac44701";
  const redirectTo = (url: string) => window.location.href = url;
  const csvJSON = (csv: string): Redirect[] =>
@@ -23,5 +23,5 @@ const fetchRedirects = async (url: string): Promise<Redirect[]> => {
     return [];
   }
 };
-const hash = window.location.hash.length ? window.location.hash.slice(1) : (redirectTo(Conveyor), "");
+const hash: string = window.location.hash.length ? window.location.hash.slice(1) : (redirectTo(Conveyor), "");
 fetchRedirects(LinksCSV).then((links) => redirectTo(links?.find((link) => link.from === hash)?.to || Conveyor));
